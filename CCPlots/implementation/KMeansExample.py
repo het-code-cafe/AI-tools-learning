254290edd40bf0f85c084dd4e4ef7350ffd9245d
Lina (Selina) Blijleven <12709907+SelinaBlijleven@users.noreply.github.com>
2025-04-17 13:03:33 +0200

    kmeans updated to provide examples with different numbers of clusters

--- CCPlots/implementation/KMeansExample.py
+++ CCPlots/implementation/KMeansExample.py
@@ -1,5 +1,8 @@
 """
 KMeansExample.py
+
+This example creates an animation and stores the final clusters as determined
+by the KMeans algorithm.
 """
 
 from sklearn.datasets import make_blobs
@@ -12,24 +15,55 @@ from CCPlots.PlotExample import PlotExample
 from CCPlots.config import CMAP_BRAND, OUTPUT_PATH, COLOR_PALETTE
 
 
+def darken_color(color, factor=0.35):
+    """
+    Darken a given RGBA color.
+
+    This function should eventually be moved to something like a helper class.
+    """
+    r, g, b, a = to_rgba(color)
+    return r * factor, g * factor, b * factor, a
+
+
 class KMeansExample(PlotExample):
 
     # We use a dark gray for the edges of the data points, so they'll be
     # visible using any brand's colours
     DARK_GRAY = COLOR_PALETTE['neutral_colors']['dark_gray']
 
+    # Store centers and scatter centrally
+    centers = None
+    scatter = None
+
     def __init__(self, n_clusters=4, n_samples=300):
+        """
+        Initialize the KMeansExample class using the desired number of clusters and samples.
+
+        :param n_clusters:  Number of clusters to generate.
+        :param n_samples:   Number of samples to generate.
+        """
         self.n_clusters = n_clusters
         self.n_samples = n_samples
-        self.X, self.y = make_blobs(n_samples=self.n_samples, centers=self.n_clusters, cluster_std=3.0, random_state=42)
-        self.kmeans = KMeans(n_clusters=self.n_clusters, init='random', n_init=1, max_iter=1, algorithm='lloyd', random_state=42)
+        self.X, self.y = make_blobs(
+            n_samples=self.n_samples,
+            centers=self.n_clusters,
+            cluster_std=3.0,
+            random_state=42)
+        self.kmeans = KMeans(
+            n_clusters=self.n_clusters,
+            init='random',
+            n_init=1,
+            max_iter=1,
+            algorithm='lloyd',
+            random_state=42)
 
         # Generate colors from the specified colormap
         cmap = plt.get_cmap(CMAP_BRAND)
         self.cluster_colors = [cmap(i / self.n_clusters) for i in range(self.n_clusters)]
-        self.center_colors = [self.darken_color(cmap(i / self.n_clusters)) for i in range(self.n_clusters)]
+        self.center_colors = [darken_color(cmap(i / self.n_clusters)) for i in range(self.n_clusters)]
 
     def main(self):
+        """ Main method to run the KMeans example. """
         fig, ax = plt.subplots()
         ax.set_xlim(-20, 20)
         ax.set_ylim(-20, 20)
@@ -50,15 +84,10 @@ class KMeansExample(PlotExample):
                             interval=10,
                             repeat=False)
         # Save the animation
-        ani.save(OUTPUT_PATH + "kmeans_animation.gif", writer='pillow')
+        ani.save(OUTPUT_PATH + f"kmeans_animation_k{self.n_clusters}.gif", writer='pillow')
 
         # Also save the last frame for non-animated use
-        fig.savefig(OUTPUT_PATH + "kmeans_clustering.png")
-
-    def darken_color(self, color, factor=0.35):
-        """Darken a given RGBA color."""
-        r, g, b, a = to_rgba(color)
-        return (r * factor, g * factor, b * factor, a)
+        fig.savefig(OUTPUT_PATH + f"kmeans_clustering_k{self.n_clusters}.png")
 
     def update(self, frame):
         # Fit KMeans incrementally for each frame
